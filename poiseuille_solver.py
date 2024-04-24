efdeb36e1d3025dc900b952c9eb3c15b589b3375
Romain Enjalbert <romainenjalbert@Romains-MBP.fritz.box>
2024-04-24 09:08:32 +0100

    Removed compression material, changed input to polyline, solution obtained through main function, changed code for 3d solution

--- poiseuille_solver.py
+++ poiseuille_solver.py
@@ -5,24 +5,8 @@ from poiseuille_class import PoiseuilleNetwork
 from argparse import ArgumentParser
 
 
-def sign(number):
-    if number > 0:
-        return 1
-    elif number < 0:
-        return -1
-    else:
-        assert 0
-
-if __name__ == "__main__":
-    parser = ArgumentParser()
-    parser.add_argument('input_edges', type=str, help='Input csv with edges')
-    parser.add_argument('input_nodes', type=str, help='Input csv with nodes')
-    parser.add_argument('input_iolets', type=str, help='Input csv with iolets')
-    parser.add_argument('h_solver', type=str, help='Haematocrit solver, choice between pries or updated_pries', choices=["pries", "updated_pries"])
-    parser.add_argument('--write_vtk', type=bool, help='Write data to vtk', default=None, required=False)
-    args = parser.parse_args()
-
-    poiseuille_class = poiseuille_reader.get_network_class(args.input_edges, args.input_nodes, args.input_iolets)
+def main(polyline_path, iolets_path):
+    poiseuille_class = PoiseuilleNetwork(polyline_path, iolets_path)
 
     p_results, Q_results, H_results, miu_results, j_results, rbc_results = [], [], [], [], [], []
     solution_converged = False
@@ -35,38 +19,52 @@ if __name__ == "__main__":
         p = poiseuille_functions.generate_solve_poiseuille_matrix(poiseuille_class, j)
         Q = poiseuille_functions.solve_flow(p, j, poiseuille_class.edges)
 
-        if iteration < 6:
+        if iteration == 1:
             Q = Q
-        elif iteration >= 6:
-            Q = [alpha * q_current + (1. - alpha) * q_previous for q_current, q_previous in zip(Q, Q_results[-1])]
         else:
-            assert 0
+            Q = [alpha * q_current + (1. - alpha) * q_previous for q_current, q_previous in zip(Q, Q_results[-1])]
 
-        convergent_bifurcations, divergent_bifurcation = poiseuille_network_functions.bifurcation_types(Q, poiseuille_class.bifurcations, poiseuille_class.edges, p)
-        new_h = poiseuille_functions.solve_haematocrit(Q, poiseuille_class, divergent_bifurcation, convergent_bifurcations, p, args.h_solver)
+        convergent_bifurcations, divergent_bifurcation = poiseuille_network_functions.bifurcation_types(Q,
+                                                                                                        poiseuille_class.bifurcations,
+                                                                                                        poiseuille_class.edges,
+                                                                                                        p)
+        new_h = poiseuille_functions.solve_haematocrit(Q, poiseuille_class, divergent_bifurcation,
+                                                       convergent_bifurcations, p)
 
         if iteration == 1:
             poiseuille_class.H = new_h
-        elif iteration > 1:
-            poiseuille_class.H = [alpha * h_current + (1. - alpha) * h_previous for h_current, h_previous in zip(new_h, H_results[-1])]
         else:
-            assert 0
+            poiseuille_class.H = [alpha * h_current + (1. - alpha) * h_previous for h_current, h_previous in
+                                  zip(new_h, H_results[-1])]
 
-        rbc_flux = [abs(q*h) for q, h in zip(Q, poiseuille_class.H)]
+        rbc_flux = [abs(q * h) for q, h in zip(Q, poiseuille_class.H)]
 
         j_results.append(j); miu_results.append(current_miu); Q_results.append(Q); H_results.append(poiseuille_class.H); p_results.append(p); rbc_results.append(rbc_flux)
+
+        poiseuille_class.Q = Q; poiseuille_class.p = p
 
         print(iteration)
         iteration += 1
 
         if iteration > 2:
-            poiseuille_reader.write_to_vtk(args.write_vtk, poiseuille_class.edges, poiseuille_class.nodes, Q_results[-1], H_results[-1], poiseuille_class.D, p_results[-1], poiseuille_class, poiseuille_class.length)
             if poiseuille_functions.check_converged(Q_results, error=0.05) is True:
                 try:
                     print("Passing convergence check, mass balance...")
-                    poiseuille_network_functions.bifurcation_check(divergent_bifurcation, convergent_bifurcations, Q, p, poiseuille_class.edges)
-                    poiseuille_network_functions.check_mass_balance(Q, poiseuille_class.H, poiseuille_class.straights, divergent_bifurcation, convergent_bifurcations, upper_bound=1.001, lower_bound=0.999)
+                    poiseuille_network_functions.bifurcation_check(divergent_bifurcation, convergent_bifurcations, Q, p,
+                                                                   poiseuille_class.edges)
+                    poiseuille_network_functions.check_mass_balance(Q, poiseuille_class.H, poiseuille_class.straights,
+                                                                    divergent_bifurcation, convergent_bifurcations,
+                                                                    upper_bound=1.001, lower_bound=0.999)
                     solution_converged = True
                 except:
                     solution_converged = False
-    poiseuille_reader.write_to_vtk(args.write_vtk, poiseuille_class.edges, poiseuille_class.nodes, Q_results[-1], H_results[-1], poiseuille_class.D, p_results[-1], poiseuille_class, poiseuille_class.length)
+    return poiseuille_class
+
+
+if __name__ == "__main__":
+    parser = ArgumentParser()
+    parser.add_argument('input_polyline', type=str, help='Input polyline with nodes, edges, and radius')
+    parser.add_argument('input_iolets', type=str, help='Input csv with iolets')
+    args = parser.parse_args()
+
+    poiseuille_reader.write_to_vtk(main(args.input_polyline, args.input_iolets))
